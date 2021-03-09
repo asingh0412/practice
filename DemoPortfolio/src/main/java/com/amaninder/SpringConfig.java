c542ca1a384c18ae9c927331b0ad1a164bbb15a1
javaexpert <javaexpert@localhost>
2021-03-09 11:04:16 -0500

    jdbc practice

---
+++ DemoPortfolio/src/main/java/com/amaninder/SpringConfig.java
@@ -0,0 +1,10 @@
+package com.amaninder;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan({"com.amaninder","com.amaninder.service"})
+public class SpringConfig {
+
+}
---
+++ DemoPortfolio/src/main/java/com/amaninder/service/AddService.java
@@ -0,0 +1,11 @@
+package com.amaninder.service;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class AddService {
+	
+	public int add(int i, int j) {
+		return i+j;
+	}
+}
